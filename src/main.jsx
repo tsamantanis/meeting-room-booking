12da683db8e9d772db8618b72b350658065f474e
tsamantanis <philip.tsamantanis@icloud.com>
2024-08-21 23:52:50 +0300

    Change reactdom render

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('meeting-room-booking-widget')).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  document.getElementById('meeting-room-booking-widget')
 )
