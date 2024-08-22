7ee43fd65c46416a7238e9d9a42e9d8b70f63efb
tsamantanis <philip.tsamantanis@icloud.com>
2024-08-22 07:48:10 +0300

    Revert reactdom root creation

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('meeting-room-booking-widget')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('meeting-room-booking-widget')
 )
