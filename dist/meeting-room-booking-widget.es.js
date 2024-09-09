a99da8b58b60784c6d27cbaa689ab8382dbe3d25
tsamantanis <philip.tsamantanis@icloud.com>
2024-09-09 13:42:50 +0300

    remove button styles

--- dist/meeting-room-booking-widget.es.js
+++ dist/meeting-room-booking-widget.es.js
@@ -19855,12 +19855,7 @@ class _4 extends HTMLElement {
   connectedCallback() {
     console.log("Development mode:", P4);
     const t = document.createElement("div");
-    this.shadowRoot.appendChild(t), this.style.position = "fixed", this.style.bottom = "20px", this.style.right = "20px", this.style.zIndex = "49";
-    {
-      const n = document.createElement("link");
-      n.rel = "stylesheet", n.href = new URL("./main.css", import.meta.url).href, this.shadowRoot.appendChild(n);
-    }
-    bl.createRoot(t).render(/* @__PURE__ */ c.jsx(N4, {}));
+    this.shadowRoot.appendChild(t), this.style.position = "fixed", this.style.bottom = "20px", this.style.right = "20px", this.style.zIndex = "49", bl.createRoot(t).render(/* @__PURE__ */ c.jsx(N4, {}));
   }
   disconnectedCallback() {
     bl.unmountComponentAtNode(this.shadowRoot);
--- src/main.jsx
+++ src/main.jsx
@@ -21,16 +21,16 @@ class MeetingRoomBookingWidget extends HTMLElement {
     this.style.bottom = '20px';
     this.style.right = '20px';
     this.style.zIndex = '49';
-    if (isDevelopment) {
-      // Development mode: Vite injects styles automatically, so we don't need to load CSS
-      console.log('Development mode: styles are injected automatically by Vite.');
-    } else {
-      // Production mode: Load the CSS file using a relative path
-      const styleLink = document.createElement('link');
-      styleLink.rel = 'stylesheet';
-      styleLink.href = new URL('./main.css', import.meta.url).href;  // Reference the CSS file relatively
-      this.shadowRoot.appendChild(styleLink);
-    }
+    // if (isDevelopment) {
+    //   // Development mode: Vite injects styles automatically, so we don't need to load CSS
+    //   console.log('Development mode: styles are injected automatically by Vite.');
+    // } else {
+    //   // Production mode: Load the CSS file using a relative path
+    //   const styleLink = document.createElement('link');
+    //   styleLink.rel = 'stylesheet';
+    //   styleLink.href = new URL('./main.css', import.meta.url).href;  // Reference the CSS file relatively
+    //   this.shadowRoot.appendChild(styleLink);
+    // }
 
     // Render the React app into the Shadow DOM's mount point
     ReactDOM.createRoot(mountPoint).render(<App />);
