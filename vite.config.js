51ce5e2401188746649431cdc0e961d71286da9d
tsamantanis <philip.tsamantanis@icloud.com>
2024-10-19 15:21:25 +0300

    Reconfigure build

--- vite.config.js
+++ vite.config.js
@@ -8,16 +8,16 @@ export default defineConfig(() => {
   return {
     plugins: [
       react(),
-      {
-        name: "configure-response-headers",
-        configureServer: (server) => {
-          server.middlewares.use((_req, res, next) => {
-            res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-            res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-            next();
-          });
-        },
-      },
+      // {
+      //   name: "configure-response-headers",
+      //   configureServer: (server) => {
+      //     server.middlewares.use((_req, res, next) => {
+      //       res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+      //       res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+      //       next();
+      //     });
+      //   },
+      // },
       env(),
     ],
     define: {
@@ -30,23 +30,23 @@ export default defineConfig(() => {
     resolve: {
       alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
     },
-    build: {
-      lib: {
-        entry: "src/main.jsx", // entry point of your component
-        name: "MeetingRoomBookingWidget", // name of your component
-        fileName: (format) => `meeting-room-booking-widget.${format}.js`,
-      },
-      rollupOptions: {
-        // Make sure to externalize dependencies that shouldn't be bundled
-        external: [],
-        output: {
-          globals: {
-            react: "React",
-            "react-dom": "ReactDOM",
-          },
-        },
-      },
-      cssCodeSplit: true, // Enable this to split CSS into a separate file
-    },
+    // build: {
+    //   lib: {
+    //     entry: "src/main.jsx", // entry point of your component
+    //     name: "MeetingRoomBookingWidget", // name of your component
+    //     fileName: (format) => `meeting-room-booking-widget.${format}.js`,
+    //   },
+    //   rollupOptions: {
+    //     // Make sure to externalize dependencies that shouldn't be bundled
+    //     external: [],
+    //     output: {
+    //       globals: {
+    //         react: "React",
+    //         "react-dom": "ReactDOM",
+    //       },
+    //     },
+    //   },
+    //   cssCodeSplit: true, // Enable this to split CSS into a separate file
+    // },
   };
 });
